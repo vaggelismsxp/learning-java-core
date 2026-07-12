9eecdccb1a89b1cd35e4a0d3ab2f475df4823886
Moschopoulos Evangelos <moschoes@Mac.home>
2026-07-12 17:16:20 +0300

    Final touches on FizzBuzz, End of core java

--- src/FizzBuzzExercise/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzzExercise/FizzBuzz/FizzBuzz.java
@@ -3,24 +3,30 @@ package TDD_FizzBuzz.FizzBuzz;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class FizzBuzz {
 
 
     public static void main (String[] args){
-        fizzBuzz(15).forEach((n)->System.out.println(n));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("FizzBuzz choose upper limit");
+        float i = scanner.nextFloat();
+        fizzBuzz(i).forEach((n)->System.out.println(n));
 
     }
 
 
     public static List<String> fizzBuzz(int n){
 
-        ArrayList<String> res = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         HashMap<Integer,String> mp = new HashMap<>();
         mp.put(3,"Fizz");
         mp.put(5, "Buzz");
+        mp.put(7, "Jazz");
 
-        int[] divisors = {3,5};
+        int[] divisors = {3,5,7};
 
         for (int i=1; i<=n;i++){
             StringBuilder s = new StringBuilder();
